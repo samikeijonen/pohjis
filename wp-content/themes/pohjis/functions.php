77e904f1a173b9427bda93d86a7de9ed36a75ad1
Sami Keijonen <sami.keijonen@foxnet.fi>
2018-03-03 10:30:54 +0200

    Whitelist content_width variable.

--- wp-content/themes/pohjis/functions.php
+++ wp-content/themes/pohjis/functions.php
@@ -102,6 +102,9 @@ add_action( 'after_setup_theme', 'pohjis_setup' );
  * @global int $content_width
  */
 function pohjis_content_width() {
+	// This variable is intended to be overruled from themes.
+	// Open WPCS issue: {@link https://github.com/WordPress-Coding-Standards/WordPress-Coding-Standards/issues/1043}.
+	// phpcs:ignore WordPress.NamingConventions.PrefixAllGlobals.NonPrefixedVariableFound
 	$GLOBALS['content_width'] = apply_filters( 'pohjis_content_width', 770 );
 }
 add_action( 'after_setup_theme', 'pohjis_content_width', 0 );
